1eca7eadfd45378e948b1732cb6bbf02d1e02900
Azra Javed <azrajaved2004@gmail.com>
2026-02-18 00:35:16 +0530

    added new DSA files'

---
+++ linear seacrch.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int search(int arr[], int n, int x) {
+        for(int i = 0; i < n; i++) {
+            if(arr[i] == x)
+                return i;   
+        }
+        return -1;         
+    }
+};
