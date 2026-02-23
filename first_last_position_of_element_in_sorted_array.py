5d7002878863d81477388890b607cf092b8e555f
Azra Javed <azrajaved2004@gmail.com>
2026-02-23 23:49:07 +0530

    python

---
+++ first_last_position_of_element_in_sorted_array.py
@@ -0,0 +1,34 @@
+class Solution:
+    def searchRange(self, nums, target):
+        
+        def findFirst():
+            left, right = 0, len(nums) - 1
+            ans = -1
+            
+            while left <= right:
+                mid = (left + right) // 2
+                
+                if nums[mid] == target:
+                    ans = mid
+                    right = mid - 1   # keep searching left
+                elif nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+                    
+            return ans
+        
+        def findLast():
+            left, right = 0, len(nums) - 1
+            ans = -1
+            
+            while left <= right:
+                mid = (left + right) // 2
+                
+                if nums[mid] == target:
+                    ans = mid
+                    left = mid + 1    # keep searching right
+                elif nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
