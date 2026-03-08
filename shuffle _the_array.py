9f1cb13ac6e81d57fa5cd00d8a72f56ba76893c3
Azra Javed <azrajaved2004@gmail.com>
2026-03-08 23:53:16 +0530

    python

---
+++ shuffle _the_array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def shuffle(self, nums, n):
+        res = []
+
+        for i in range(n):
+            res.append(nums[i])
+            res.append(nums[i+n])
+
+        return res
