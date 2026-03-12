4409bfa337635097b9f4f54b2e73807d2c980b05
Azra Javed <azrajaved2004@gmail.com>
2026-03-12 10:12:58 +0530

    python

---
+++ fruits_into_baskets.py
@@ -0,0 +1,19 @@
+class Solution:
+    def totalFruit(self, fruits):
+        basket = {}
+        left = 0
+        max_fruits = 0
+
+        for right in range(len(fruits)):
+            fruit = fruits[right]
+            basket[fruit] = basket.get(fruit, 0) + 1
+
+            while len(basket) > 2:
+                basket[fruits[left]] -= 1
+                if basket[fruits[left]] == 0:
+                    del basket[fruits[left]]
+                left += 1
+
+            max_fruits = max(max_fruits, right - left + 1)
+
+        return max_fruits
